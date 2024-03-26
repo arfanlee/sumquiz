9ce666facf7be5303ea2a4b95a1960e3d06ecb5d
Arfan Lee <arfanlee@outlook.com>
2024-03-26 23:16:03 +0800

    Implemented summation pdf parser and summation function

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 import streamlit as st
 from method_class import sumquiz as sq
+from openai import OpenAI
 
-st.title("Welcome to SumMemory App!")
+client = OpenAI(
+    api_key=st.secrets["OPENAI_API_KEY"]
+)
 
-st.divider()
+def main():
+	st.title("Welcome to SumMemory App!")
 
-st.header("About")
-about = "SumMemory allows you to ask questions about your documents and get accurate answers with instant citations.  You can use it to summarize a paper or test you memory with quizzes."
+	st.divider()
 
-st.write(about)
+	st.header("About")
+	about = "SumMemory allows you it to summarize a file (pdf only) or test your memory with a simple quiz."
 
-st.header("How to use")
+	st.write(about)
 
-instruction = """
+	st.header("How to use")
 
-    - Select which function do you want to use in the sidebar.
-    - Upload a pdf, docx, or txt file📄 (Currently we don't support scanned PDF)
-    - For summary, you can prompt for other query, for example, you can ask the MeryGPT 
-      to explain a certain topic like you're a 5 years old.
-    - For quiz, it would ask a random question from the syllabus, and waiting
-      for you to answer.
-"""
+	instruction = """
 
-st.markdown(instruction)
+		1. Select which function do you want to use in the sidebar.
+		2. Upload a pdf, docx, or txt file📄 (Currently we don't support scanned PDF)
+		3. For summary, you can prompt for other query, for example, you can ask the MeryGPT 
+		to explain a certain topic like you're a 5 years old.
+		4. For quiz, it would ask a random question from the syllabus, and waiting
+		for you to answer.
+	"""
 
-selected = st.selectbox("Pick one", ["Summary", "Quiz"], index=None, placeholder="Select a function...")
+	st.markdown(instruction)
 
-if selected == "Summary":
-    sq.summation_show()
+	uploaded_file = sq.file_exporter()
 
-elif selected == "Quiz":
-    sq.quiz_show()
+	if uploaded_file is not None:
+		if uploaded_file.name.endswith(".pdf"):
+			parsed = sq.parse_pdf(uploaded_file)
+		else:
+			raise ValueError("File type not supported!")
+
+	selected = st.selectbox("Pick one", ["Summary", "Quiz"], index=None, placeholder="Select a function...")
+
+	if selected == "Summary":
+		response = sq.summation(client, parsed)
+		st.write(response)
+		query = st.text_input("Query", placeholder="E.g. Explain further like I'm 5")
+		# st.button("Submit")
+
+		if st.button("Submit"):
+			sq.summation_ask(client, query, parsed)
+
+	elif selected == "Quiz":
+		sq.quiz_show()
+
+
+if __name__ == "__main__":
+    main()
--- method_class.py
+++ method_class.py
@@ -1,36 +1,113 @@
 import streamlit as st
+import re
+from io import BytesIO
+from pypdf import PdfReader
 
 class sumquiz:
     def file_exporter():
         
         file = st.file_uploader(
-            "Upload a .pdf, .docx or .txt file.",
-            type=["pdf", "docx", "txt"],
+            "Upload a pdf file!",
+            type=["pdf"],
             help="Scanned documents are not supported yet!")
         
         return file
 
-    def summation_show():
+    def summation(client, parsed_pdf):
         st.title("Summary")
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role":"system",
+                "content": "You're an adept summarizer. You will summarize the text given to a proper reading material for a student."
+            },
+            {
+                "role":"user",
+                "content":"""Summarize this: Machine learning is a subfield of artificial intelligence (AI) that focuses on the development of algorithms and 
+                statistical models that enable computers to perform tasks without explicit programming. At its core, machine learning is about creating systems 
+                that can learn from data, identify patterns, and make decisions or predictions based on that data. It represents a paradigm shift in how we 
+                approach problem-solving, shifting from traditional rule-based programming to systems that can learn and adapt from experience. The roots of 
+                machine learning can be traced back to the early days of computing, but it has gained significant traction and prominence in recent decades, 
+                fueled by advancements in computational power, the availability of large datasets, and innovations in algorithmic techniques. Today, machine 
+                learning is driving transformative change across a wide range of industries and applications, from finance and healthcare to transportation and 
+                entertainment."""
+            },
+            {
+                "role":"assistant",
+                "content":"""Machine learning, a branch of AI, enables computers to perform tasks without explicit programming by developing algorithms and models 
+                that learn from data to make predictions or decisions. It marks a shift from rule-based programming to systems that learn from experience. Dating 
+                back to early computing, it has surged in recent years due to advancements in computation, data availability, and algorithms. Today, it drives 
+                innovation across industries like finance, healthcare, and entertainment."""
+            },
+            {
+                "role":"user",
+                "content": parsed_pdf
+            }],
+            max_tokens=400,
+            temperature=1.3
+        )
 
-        st.write("""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. Blandit turpis cursus in hac habitasse platea. Nulla
-                pellentesque dignissim enim sit amet venenatis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Pulvinar elementum i
-                nteger enim neque volutpat ac tincidunt. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Sed libero enim sed faucibus 
-                turpis in. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Pulvinar mattis nunc sed blandit libero """)
+        parsed_content = response.choices[0].message.content
 
-        st.text_input("Query", placeholder="E.g. Explain further like I'm 5")
+        st.write(parsed_content)
+        
+    def summation_ask(client, query, content):
+        response=client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role":"system",
+            "content": "You're a excellent responder. You will answer the query with the provided info in the sentence."
+        },
+        {
+            "role":"user",
+            "content":"""What is the root of machine learning according to this sentence?: Machine learning is a subfield of artificial intelligence (AI) 
+            that focuses on the development of algorithms and  statistical models that enable computers to perform tasks without explicit programming. At 
+            its core, machine learning is about creating systems  that can learn from data, identify patterns, and make decisions or predictions based on 
+            that data. It represents a paradigm shift in how we  approach problem-solving, shifting from traditional rule-based programming to systems that 
+            can learn and adapt from experience. The roots of  machine learning can be traced back to the early days of computing, but it has gained significant 
+            traction and prominence in recent decades, fueled by advancements in computational power, the availability of large datasets, and innovations in
+            algorithmic techniques. Today, machine learning is driving transformative change across a wide range of industries and applications, from finance
+            and healthcare to transportation and entertainment."""
+        },
+        {
+            "role":"assistant",
+            "content":"""The origins of machine learning can be linked to the initial stages of computing, yet its notable rise to prominence has occurred more 
+            recently, driven by advancements in computational capabilities, the accessibility of vast datasets, and breakthroughs in algorithmic methodologies."""
+        },
+        {
+            "role":"user",
+            "content":f"Answer this question: '{query}'' based on the content '{content}'"
+        }]
+        )
 
-        st.button("Submit")
+        query_answered = response.choices[0].message.content
+
+        st.header("Answer")
+        st.write(query_answered)
 
     def quiz_show():
         st.title("Quiz Time!")
-
-        vector_index = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna?"""
-
-        st.write(vector_index)
 
         # st.text_input("Answer")
         query = st.text_area("Ask a question about the document.")
 
         st.button("Submit")
+    
+    def parse_pdf(file: BytesIO):
+        pdf = PdfReader(file)
+        output = []
+        for page in pdf.pages:
+            text = page.extract_text()
+            # Merge hyphenated words
+            text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+            # Fix newlines in the middle of sentences
+            text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+            # Remove multiple newlines
+            text = re.sub(r"\n\s*\n", "\n\n", text)
+            # Remove unicode characters
+            text = re.sub(r'[^\x00-\x7F]+', '', text)
+
+            output.append(text)
+        
+        output_str = ' '.join(output)
+        return output_str
