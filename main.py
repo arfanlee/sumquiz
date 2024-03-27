f64789d85d47364d62f069263811ead8284ac424
Arfan Lee <arfanlee@outlook.com>
2024-03-27 08:41:22 +0800

    Removed quiz function

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ client = OpenAI(
 )
 
 def main():
-	st.set_page_config(page_title="SumQuiz", page_icon="📖", layout="wide")
-	st.header("📖 SumQuiz App")
+	st.set_page_config(page_title="SumGPT", page_icon="📖", layout="wide")
+	st.header("📖 SumGPT App")
 	# st.title("Welcome to SumMemory App!")
 
 	st.divider()
 
-	col1, col2 =  st.columns(2, gap="large")
+	col1, col2 = st.columns(2, gap="large")
 	with col1:
 		st.header("About")
-		about = "SumMemory allows you it to summarize a file (pdf only) or test your memory with a simple quiz."
+		about = "SumGPT allows you it to summarize a file (pdf only) and ask it anything about the document."
 
 		st.write(about)
 
@@ -24,12 +24,9 @@ def main():
 
 		instruction = """
 
-			1. Select which function do you want to use in the sidebar.
-			2. Upload a pdf, docx, or txt file📄 (Currently we don't support scanned PDF)
-			3. For summary, you can prompt for other query, for example, you can ask the MeryGPT 
-			to explain a certain topic like you're a 5 years old.	
-			4. For quiz, it would ask a random question from the syllabus, and waiting
-			for you to answer.
+			2. Upload a pdf file📄 (Currently we don't support scanned PDF)
+			3. For summary, you can prompt for other query, for example, you can ask the SumGPT 
+			to explain a certain topic like you're a 5 years old.
 		"""
 
 		st.markdown(instruction)
@@ -46,19 +43,15 @@ def main():
 				else:
 					raise ValueError("File type not supported!")
 
-			selected = st.selectbox("Pick one", ["Summary", "Quiz"], index=None, placeholder="Select a function...")
+			response = sq.summation(client, parsed)
+			st.write(response)
+			query = st.text_input("Query", placeholder="E.g. Explain further like I'm 5")
 
-			if selected == "Summary":
-				response = sq.summation(client, parsed)
-				st.write(response)
-				query = st.text_input("Query", placeholder="E.g. Explain further like I'm 5")
-				# st.button("Submit")
-
-				if st.button("Submit"):
-					sq.summation_ask(client, query, parsed)
-
-			elif selected == "Quiz":
-				sq.quiz_show(client, parsed)
+			if st.button("Submit"):
+				query_answered = sq.summation_ask(client, query, parsed)
+				st.header("Answer")
+				st.write(query_answered)
+				st.stop()
 
 
 if __name__ == "__main__":
--- method_class.py
+++ method_class.py
@@ -2,7 +2,6 @@ import streamlit as st
 import re
 from io import BytesIO
 from pypdf import PdfReader
-import ast
 
 class sumquiz:
     def file_exporter():
@@ -13,6 +12,25 @@ class sumquiz:
             help="Scanned documents are not supported yet!")
         
         return file
+    
+    def parse_pdf(file: BytesIO):
+        pdf = PdfReader(file)
+        output = []
+        for page in pdf.pages:
+            text = page.extract_text()
+            # Merge hyphenated words
+            text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+            # Fix newlines in the middle of sentences
+            text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+            # Remove multiple newlines
+            text = re.sub(r"\n\s*\n", "\n\n", text)
+            # Remove unicode characters
+            text = re.sub(r'[^\x00-\x7F]+', '', text)
+
+            output.append(text)
+        
+        output_str = ' '.join(output)
+        return output_str
 
     def summation(client, parsed_pdf):
         st.title("Summary")
@@ -49,8 +67,7 @@ class sumquiz:
         )
 
         parsed_content = response.choices[0].message.content
-
-        st.write(parsed_content)
+        return parsed_content
         
     def summation_ask(client, query, content):
         response=client.chat.completions.create(
@@ -82,97 +99,4 @@ class sumquiz:
         )
 
         query_answered = response.choices[0].message.content
-
-        st.header("Answer")
-        st.write(query_answered)
-
-    def quiz_show(client, content):
-        st.title("Quiz Time!")
-
-        response=client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{
-            "role":"system",
-            "content": "You're a excellent quizzer. You will ask the user with the provided info in the sentence."
-        },
-        {
-            "role":"user",
-            "content":"""Ask a quick quiz from this information and an answer to it in a Python list, ['Question', 'Answer'] e.g. '['What is AI', 'AI stands for Artificial Intelligence']': 
-            Artificial intelligence is a rapidly evolving field in computer science that explores the creation of intelligent machines capable of mimicking human 
-            cognitive functions, including learning, problem-solving, and decision-making."""
-        },
-        {
-            "role":"assistant",
-            "content":"['In which industries can machine learning be used in?', 'Machine learning can applied in finance and healthcare to transportation and entertainment.']"
-        },
-        {
-            "role":"user",
-            "content":f"Create a a question based on this information '{content}'"
-        }]
-        )
-
-        question = response.choices[0].message.content
-        question = ast.literal_eval(question)
-        ask = question[0]
-        ai_answer = question[1]
-        print(ai_answer)
-        # print(type(ai_answer))
-
-        st.header("Question")
-        st.write(ask)
-        with st.form(key="qa_form"):
-            user_answer = st.text_area("Answer the question")
-            submit = st.form_submit_button("Submit")
-
-        if submit:
-            st.write(compare_answer(client, ai_answer, user_answer))
-    
-    def parse_pdf(file: BytesIO):
-        pdf = PdfReader(file)
-        output = []
-        for page in pdf.pages:
-            text = page.extract_text()
-            # Merge hyphenated words
-            text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
-            # Fix newlines in the middle of sentences
-            text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
-            # Remove multiple newlines
-            text = re.sub(r"\n\s*\n", "\n\n", text)
-            # Remove unicode characters
-            text = re.sub(r'[^\x00-\x7F]+', '', text)
-
-            output.append(text)
-        
-        output_str = ' '.join(output)
-        return output_str
-    
-def compare_answer(client, ai_answer, user_answer):
-    response=client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{
-            "role":"system",
-            "content": "You're a excellent teacher. You will compare the answer from user with the correct answer."
-        },
-        {
-            "role":"user",
-            "content":"""Does the answer: AI stands for Artificial Imagination. Equivalent with correct answer: AI stands for Artificial Intelligence."""
-        },
-        {
-            "role":"assistant",
-            "content":"Wrong. AI stands for Artificial Intelligence not Artificial Imagination.."
-        },
-        {
-            "role":"user",
-            "content":"""Does the answer: AI stands for Artificial Intelligence. Equivalent with correct answer: AI stands for Artificial Intelligence."""
-        },
-        {
-            "role":"assistant",
-            "content":"Correct. AI is indeed stands for Artificial Intelligence."
-        },
-        {
-            "role":"user",
-            "content":f"Is the answer: {user_answer} equivalent with the correct answer {ai_answer}"
-        }]
-        )
-    compared_answer = response.choices[0].message.content
-    return compared_answer
+        return query_answered
